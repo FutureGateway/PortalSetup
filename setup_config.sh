7159b3cbb8d850beaf78ebabe517b29e4fbe84a9
Riccardo Bruno <riccardo.bruno@gmail.com>
2015-10-02 12:02:55 +0200

    code shape changes
    get_ts and get_file functions in config script, since it is sourced by
    all setup scripts

--- setup_config.sh
+++ setup_config.sh
@@ -14,8 +14,43 @@ FGREPO=$FGHOME/FGRepo                       # Files could be cached into this re
 FGLOCATION=$FGHOME/FutureGateway            # Location of the FutureGateway installation
 FGENV=$FGLOCATION/setenv.sh                 # FutureGateway environment variables
 
+# Function that produces a timestamp
+get_ts() {
+ TS=$(date +%y%m%d%H%M%S)
+}
 
-
-
+# Function that retrieves a file from FGRepo or download it
+# from the web. The function takes three arguments:
+#   $1 - Source URL
+#   $2 - Destination path; (current dir if none; or only path to destination)
+#   $3 - Optional the name of the file (sometime source URL does not contain the name)
+# FGREPO directory exists, because created by the preinstall_fg
+get_file() {
+  if [ "${3}" != "" ]; then
+    FILENAME="${3}"
+  else
+    FILENAME=$(basename $1)
+  fi
+  if [ "${2}" != "" ]; then
+    DESTURL="${2}"
+  else
+    DESTURL=$(pwd)
+  fi
+  if [ -e "${FGREPO}/${FILENAME}" ]; then
+    # The file exists in the cache
+    echo "File ${FILENAME} exists in the cache" 
+    cp "${FGREPO}/${FILENAME}" $DESTURL/$FILENAME
+  else
+    echo "File ${FILENAME} not in cache; retrieving it from the web"
+    wget "${1}" -O $FGREPO/$FILENAME 2>/dev/null
+    RES=$?
+    if [ $RES -ne 0 ]; then
+      echo "FATAL: Unable to download from URL: ${1}"
+      rm -f $FGREPO/$FILENAME
+      exit 1
+    fi 
+    cp "${FGREPO}/${FILENAME}" $DESTURL/$FILENAME
+  fi
+}
 
 
